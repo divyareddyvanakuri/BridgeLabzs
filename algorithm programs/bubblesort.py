2ded3db6d1a5480cca2dc40f239b12e1e718d763
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-08 10:50:38 +0530

    [Divya] add.Secondcommit of bubblesort

--- algorithm programs/bubblesort.py
+++ algorithm programs/bubblesort.py
@@ -1,6 +1,6 @@
-fruits = ['grape', 'banana', 'strawberry', 'apple', 'peach', 'cherry']
-for j in range(len(fruits)-1,0,-1):
+my_list = [56,2,-67,47,38,0,33]
+for j in range(len(my_list)-1,0,-1):
     for i in range(j):
-        if fruits[i]>fruits[i+1]:
-            fruits[i],fruits[i+1]=fruits[i+1],fruits[i]
-print(fruits)
+        if my_list[i]>my_list[i+1]:
+            my_list[i],my_list[i+1]=my_list[i+1],my_list[i]
+print(my_list)
