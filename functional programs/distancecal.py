21c4fb4923966df7eac050fb371f120fb3366575
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-08 09:58:33 +0530

    [Divya]add.Distance calculation

---
+++ functional programs/distancecal.py
@@ -0,0 +1,5 @@
+import math
+x = int(input("enter x axis value:"))
+y = int(input("enter y axis value:"))
+cal = math.sqrt(x*x+y*y)
+print("Euclidean distance from the point (x, y) to the origin (0, 0)",cal)
