c906e8d3c26bcb62e0f64007a5752b5512b9288a
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-08 15:52:11 +0530

    [Divya] add.tobinary function

--- junit testing programs/tobinary.py
+++ junit testing programs/tobinary.py
@@ -1,12 +1,5 @@
-x =format(100,'08b')
-lst = list(x)
-n = len(lst)//2
-lst1 = lst[n:]
-lst2 = lst[:n]
-new_list = lst1+lst2
-c = " "
-for i in new_list:
-    c += i
-num = int(c,2)
+def Tobinary(n):
+    return int(n,2)
+num = format(106,'0b')
 print(num)
-    
+print(Tobinary(num))
