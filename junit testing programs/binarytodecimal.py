da2ed279c9061754a0fc5e04b159748d0d62ccab
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-08 15:54:00 +0530

    [Divya] add.binarytodecimal convsersion

--- junit testing programs/binarytodecimal.py
+++ junit testing programs/binarytodecimal.py
@@ -1,5 +1,12 @@
-def binarytodecimal(n):
-    return int(n,2)
-num = format(106,'0b')
+x =format(100,'08b')
+lst = list(x)
+n = len(lst)//2
+lst1 = lst[n:]
+lst2 = lst[:n]
+new_list = lst1+lst2
+c = " "
+for i in new_list:
+    c += i
+num = int(c,2)
 print(num)
-print(binarytodecimal(num))
+    
