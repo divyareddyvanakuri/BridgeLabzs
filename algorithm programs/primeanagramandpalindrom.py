a59d4189305b89268b36fddf0d87350aeef68a58
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-08 11:51:47 +0530

    [Divya]add.prime anagram and palindrome

---
+++ algorithm programs/primeanagramandpalindrom.py
@@ -0,0 +1,28 @@
+lower = 0
+upper = 100
+my_list=[]
+for val in range(lower,upper+1):
+    if val>1:
+        for n in range(2,val):
+            if (val%n) == 0:
+                break
+        else:
+            my_list.append(val)
+#print(my_list)
+def anagramnumbers(my_list):
+    anagram_list=[]
+    for i in my_list:
+        for j in my_list:
+            if (i != j) and  (sorted(str(i))==sorted(str(j))):
+                anagram_list.append(i) 
+    return anagram_list 
+print(anagramnumbers(my_list))
+def palindromenumbers(my_list):
+    palindrome_list=[]
+    for i in my_list:
+            if(str(i)==str(i)[::-1]):
+                palindrome_list.append(i) 
+    return palindrome_list 
+print(palindromenumbers(my_list))
+ 
+
