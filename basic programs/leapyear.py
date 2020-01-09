fe680220f12a52557496a6919b404f4284d5b65c
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-09 17:22:23 +0530

    [Divya]add.Leap year

--- basic programs/leapyear.py
+++ basic programs/leapyear.py
@@ -1,12 +1,20 @@
 # Leap Year
 def Leapyear(year):
-    return ((year%4==0)and(year%100!=0)or(year%400==0))
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False 
 year = int(input("enter year:"))
 if len(str(year))<4:
     print("please enter four digited integer value")
 else:
-    Leapyear(year)
-    if year == True:
+    if Leapyear(year) == True:
         print("leap year")
     else:
         print("not a leap year")
