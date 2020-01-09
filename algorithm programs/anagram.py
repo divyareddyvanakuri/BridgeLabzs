36c8e1a37b74ab5c668322f047ecb434b6535c7b
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-09 16:56:31 +0530

    [Divya] add.anagram

--- algorithm programs/anagram.py
+++ algorithm programs/anagram.py
@@ -1,5 +1,5 @@
-string1 = "earth"
-string2 = "heart"
+string1 = input("enter string1:")
+string2 = input("enter string2:")
 if sorted(string1)==sorted(string2):
     print("entered strings are anagram to each other")
 else:
