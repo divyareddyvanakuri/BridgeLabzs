7d99b7e022341bc0a0f35d88f946f335ec55f797
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-09 16:09:07 +0530

    [Divya]add.replace the string with username

--- basic programs/prntusername.py
+++ basic programs/prntusername.py
@@ -3,6 +3,6 @@ username = input("enter username: ")
 if len(username)<3:
     print("Ensure username minimum three characters")
 else:
-    string = "Hello <<username>>,How are you?"
-    f = re.sub("<<username>>",username,string)
+    string = "Hello <username>,How are you?"
+    f = re.sub("<username>",username,string)
     print(f)
