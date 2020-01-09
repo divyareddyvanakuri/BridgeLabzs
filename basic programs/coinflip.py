6a71f58d269e4f99284eb8b85f112daedb0a2098
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-09 17:02:16 +0530

    [Divya]add.flip coin and percentage of heads and tails

--- basic programs/coinflip.py
+++ basic programs/coinflip.py
@@ -1,5 +1,5 @@
 import random
-def fun(tries):
+def coinflips(tries):
     hcount = 0
     tcount = 0
     phead = 0
@@ -7,15 +7,15 @@ def fun(tries):
     flips = 0
     while flips<tries:
         flips += 1
-        coin = random.random() #Return a random integer N such that a <= N <= b. 
+        coin = random.random()  # Random float:  0.0 <= x < 1.0
         if coin>0.5:
             hcount += 1
         else:
             tcount += 1
     phead = hcount/ flips*100
     ptail = 100-phead
-    print("Total no of flips",flips)
-    print("percentage of head",phead)
-    print("percentage of tail",ptail)
+    print("Total no of flips:",flips)
+    print("percentage of head:",phead)
+    print("percentage of tail:",ptail)
 tries = int(input("enter tries:"))
-fun(tries)
+coinflips(tries)
