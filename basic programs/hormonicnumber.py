9a9b0a16bf7bac6aa5522bfdd64cc5cb2f67f22a
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-09 17:10:55 +0530

    [Divya]add.harmonicnumber

--- basic programs/hormonicnumber.py
+++ basic programs/hormonicnumber.py
@@ -2,4 +2,4 @@ num = int(input("enter a harmonic number: "))
 harmonic = 1
 for i in range(2,num+1):
     harmonic += 1/i
-print("{0}th harmonic value:{1} ".format(num,harmonic))
+    print("{0}th harmonic value:{1} ".format(i,harmonic))
