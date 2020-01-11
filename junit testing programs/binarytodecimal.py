455602c07d0244235e995328b97aac9379153248
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-11 14:34:33 +0530

    [Divya].add Decimal conversion

--- junit testing programs/binarytodecimal.py
+++ junit testing programs/binarytodecimal.py
@@ -1,12 +1,11 @@
-x =format(100,'08b')
-lst = list(x)
-n = len(lst)//2
-lst1 = lst[n:]
-lst2 = lst[:n]
-new_list = lst1+lst2
-c = " "
-for i in new_list:
-    c += i
-num = int(c,2)
-print(num)
-    
+def decimalconversion(z):
+    x =format(z,'08b')
+    lst = list(x)
+    n = len(lst)//2
+    lst1 = lst[n:]
+    lst2 = lst[:n]
+    new_list = lst1+lst2
+    c = " "
+    for i in new_list:
+        c += i
+    return int(c,2)
