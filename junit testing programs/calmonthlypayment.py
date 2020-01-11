55eeb6886cbe27e44795ca289cfa5def98daf8fe
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-11 14:33:35 +0530

    [Divya].add calculation of monthly payment

--- junit testing programs/calmonthlypayment.py
+++ junit testing programs/calmonthlypayment.py
@@ -1,7 +1,4 @@
-p = float(input("enter principal loan amount:"))
-y = int(input("enter make over years:"))
-R = float(input("enter monthly rate of interest:"))
-n = 12 * y
-r = R /(12*100)
-payment = round(p*r/(1-(1+r)**(-n)),3)
-print(payment)
+def payment(p,y,R):
+    n = 12 * y
+    r = R /(12*100)
+    return int(p*r/(1-(1+r)**(-n)))
