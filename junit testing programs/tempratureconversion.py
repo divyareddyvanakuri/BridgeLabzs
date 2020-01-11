e4fefac037cd5e4e6168be9353c47c478230c8d3
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-11 14:37:10 +0530

    [Divya].add tempratureconversion

--- junit testing programs/tempratureconversion.py
+++ junit testing programs/tempratureconversion.py
@@ -1,16 +1,19 @@
-print("Temprature conversion main-menu")
-print("1-convert from celsius to fahranheit")
-print("2-convert form fahranheit to celsius")
-option = int(input("choose an option 1 or 2:"))
-class tconversion:
-    def __init__(self,temprature):
-        self.temprature = temprature
-    def fcon(self):
-        return round((self.temprature * 9/5)+32,3)
-    def ccon(self):
-        return round((self.temprature - 32)*5/9,3)
-t = tconversion(int(input("enter temprature value:")))
-if option == 1:
+#print("Temprature conversion main-menu:")
+#print(" 1-convert from celsius to fahranheit")
+#print(" 2-convert form fahranheit to celsius")
+#option = int(input("choose an option 1 or 2:"))
+#class tconversion:
+    #def __init__(self,temprature):
+        #self.temprature = temprature
+def fcon(temprature):
+    return int((temprature * 9/5)+32)
+def ccon(temprature):
+    return int((temprature - 32)*5/9)
+'''if option == 1:
+    t = tconversion(int(input("enter temprature value:")))
     print("Temprature:",t.fcon(),"degrees fahranheit")
-else:
+elif option==2:
+    t = tconversion(int(input("enter temprature value:")))
     print("Temprature:",t.ccon(),"degrees celsius")
+else:
+    print("please enter choose in between 1 or 2")'''
