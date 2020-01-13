35142aec2d4b836e138bca9b56f5fb9212d21e4f
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-13 14:53:23 +0530

    [Divya]add.Temprature conversion

--- junit testing programs/tempratureconversion.py
+++ junit testing programs/tempratureconversion.py
@@ -1,19 +1,5 @@
-#print("Temprature conversion main-menu:")
-#print(" 1-convert from celsius to fahranheit")
-#print(" 2-convert form fahranheit to celsius")
-#option = int(input("choose an option 1 or 2:"))
-#class tconversion:
-    #def __init__(self,temprature):
-        #self.temprature = temprature
 def fcon(temprature):
     return int((temprature * 9/5)+32)
 def ccon(temprature):
     return int((temprature - 32)*5/9)
-'''if option == 1:
-    t = tconversion(int(input("enter temprature value:")))
-    print("Temprature:",t.fcon(),"degrees fahranheit")
-elif option==2:
-    t = tconversion(int(input("enter temprature value:")))
-    print("Temprature:",t.ccon(),"degrees celsius")
-else:
-    print("please enter choose in between 1 or 2")'''
+
