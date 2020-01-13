51f1c3e92235eba72834fc631b74863b7c1e8a89
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-13 15:04:22 +0530

    [Divya]add.coinflip and percentage of loss and win

--- basic programs/coinflip.py
+++ basic programs/coinflip.py
@@ -2,8 +2,6 @@ import random
 def coinflips(tries):
     hcount = 0
     tcount = 0
-    phead = 0
-    ptail = 0
     flips = 0
     while flips<tries:
         flips += 1
@@ -14,7 +12,6 @@ def coinflips(tries):
             tcount += 1
     phead = round(hcount/ flips*100,3)
     ptail = 100-phead
-    print("Total no of flips:",flips)
     print("percentage of head:",phead)
     print("percentage of tail:",ptail)
 tries = int(input("enter tries:"))
