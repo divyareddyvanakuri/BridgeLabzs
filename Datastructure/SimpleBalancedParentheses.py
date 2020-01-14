ee7aa5ab4513ece7f5e048e6d64306a3aeba25b2
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-14 18:24:29 +0530

    [Divya]Refactor:modifed the push,pop and balanced parentheses operations

--- Datastructure/SimpleBalancedParentheses.py
+++ Datastructure/SimpleBalancedParentheses.py
@@ -1,6 +1,10 @@
 class Stack:
     def __init__(self,stack=[]):
         self.stack=stack
+    def push(self,item):
+        self.stack.append(item)
+    def Pop(self):
+        self.stack.pop()
     def is_empty(self):
         return self.stack ==[]
     def peek(self):
@@ -10,26 +14,21 @@ class Stack:
         return self.stack
     def size(self):
         return len(self.stack)
-    def Check_for_balance(self,items):
-        self.items=items
-        n = len(self.items)
-        for i in range(n):
-            if self.items[i]=="(":
-                self.stack.append(a[i])
-            elif self.items[i]==")":
-                if len(self.items)>0:
-                    self.stack.pop()
-        if len(self.stack)==0:
-            return "balanced"
-        else:
-            return "Unbalanced"
 mystack = Stack()
-a = "(5+6)∗(7+8)/((4+3)(5+6)∗(7+8)/(4+3)"
-items=[]
+a = input("Take Any Arithmetic Expression or Parentheses Expression:")
+print("Input:",a)
+open_list = ["(","[","{"]
+closed_list = [")","]","}"]
 for i in range(len(a)):
-    items.append(a[i])
-print(mystack.Check_for_balance(items))
-print(mystack.peek())
-print(mystack.is_empty())
-print(mystack.size())
+    if a[i] in open_list:
+        mystack.push(a[i])
+    elif a[i] in closed_list:
+        if mystack.size()>0:
+            mystack.Pop()
+if mystack.is_empty():
+    print("balanced")
+else:
+    print("unbalanced")
+    print(mystack.peek())
+print(mystack.get_stack())
                 
