45acd8bcb68ecfbe30c4461a19dc1c8a715e4527
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-16 10:41:04 +0530

    [Divya]add:people to queue to Deposit or withdraw Money

--- Datastructure/SimulateBankingCashCounter.py
+++ Datastructure/SimulateBankingCashCounter.py
@@ -8,13 +8,55 @@ class Queue():
         del self.queue[0]
     def get_queue(self):
         return self.queue
-
+    def isEmpty(self):
+        return self.queue==[]
+    def size(self):
+        return len(self.queue)
 myqueue = Queue()
-myqueue.push(7)
-myqueue.push(8)
-myqueue.push(9)
-myqueue.push(10)
-myqueue.push(11)
-print(myqueue.get_queue())
-myqueue.pop()
-print(myqueue.get_queue())
+Names = ["kai","suho","minho"]
+cash = [1000,2000,3000]
+for i,j in zip(Names,cash):
+    print(i,":",j)
+print("Cash Counter Menu:")
+print(" 1.Cash withdraw")
+print(" 2.Cash Deposit")
+
+customer_name=input("enter User Name:")
+if customer_name in Names:
+    pos = Names.index(customer_name)
+    Choice = int(input("select between 1 or 2:"))
+    if Choice == 1:
+        if customer_name in Names:
+            amount = float(input("Enter withdraw amount:"))
+            if 100<amount<cash[pos]:
+                cash[pos] -= amount
+                amount = cash[pos]
+                cash.insert(pos,amount)
+                print("Balance Amount")
+                print(Names[pos],":",cash[pos])
+            else:
+                print("Ensure about amount of Money in your Account")
+                print(Names[pos],":",cash[pos])
+        else:
+            print("Ensure user name needs to be valide one")
+    elif Choice == 2:
+        if customer_name in Names:
+            amount = float(input("Enter Deposit amount:"))  
+            cash[pos] += amount
+            print("Balance Amount")
+            print(Names[pos],":",cash[pos])
+        else:
+            print("Ensure user name needs to be valide one")
+    else:
+        print("please select valide choice")
+else:
+    print("User Name was Invalide")
+    print()
+    new_account_acceptance = input("you want to creat New Account in our Bank y or n:")
+    if new_account_acceptance.lower()=='y':
+        Names.append(customer_name)
+        amount = float(input("Enter Amount of Money you want Deposit in your New Account:"))
+        cash.append(amount)
+    else:
+        print("Have A Day")
+
