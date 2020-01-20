c8d6d2fffabd090ee9c58545aa6a8fb003390bc2
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-20 13:06:13 +0530

    [Divya] Add: the function logic of 2D array of anagramprimenumbers in that range

---
+++ Datastructure/primeanagramsin2Darray.py
@@ -0,0 +1,25 @@
+lower = int(input("enter lower range value:"))
+upper = int(input("enter lower range value:"))
+
+def primeanagrams(lower,upper):
+    prime_list=[]
+    for val in range(lower,upper+1):
+        if val>1:
+            for n in range(2,val):
+                if (val%n) == 0:
+                    break
+            else:
+                prime_list.append(val)
+    anagram_list=[]
+    for i in prime_list:
+        for j in prime_list:
+            if i != j and sorted(str(i))==sorted(str(j)):
+                anagram_list.append(j) 
+    return anagram_list 
+mylist=primeanagrams(lower,upper)
+TwoDArray_anagramprime = [[x for x in range(100) if x in mylist],[x for x in range(100,200) if x in mylist],
+    [x for x in range(200,300) if x in mylist],[x for x in range(300,400) if x in mylist],
+    [x for x in range(400,500) if x in mylist],[x for x in range(500,600) if x in mylist],
+    [x for x in range(600,700) if x in mylist],[x for x in range(800,900) if x in mylist],
+    [x for x in range(900,1000) if x in mylist]]
+print(TwoDArray_anagramprime)
