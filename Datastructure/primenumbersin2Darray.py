3378a60315ab334166196db4f2f12f272509a290
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-20 13:04:47 +0530

    [Divya] Add: the function logic of 2D array of primenunbers in that range

--- Datastructure/primenumbersin2Darray.py
+++ Datastructure/primenumbersin2Darray.py
@@ -10,9 +10,9 @@ for number in range(lower,upper):
            mylist.append(number)
 #print(mylist)
 
-A = [[x for x in range(100) if x in mylist],[x for x in range(100,200) if x in mylist],
+TwoDArray_primenumbers = [[x for x in range(100) if x in mylist],[x for x in range(100,200) if x in mylist],
     [x for x in range(200,300) if x in mylist],[x for x in range(300,400) if x in mylist],
     [x for x in range(400,500) if x in mylist],[x for x in range(500,600) if x in mylist],
     [x for x in range(600,700) if x in mylist],[x for x in range(800,900) if x in mylist],
     [x for x in range(900,1000) if x in mylist]]
-print(A)
+print(TwoDArray_primenumbers)
