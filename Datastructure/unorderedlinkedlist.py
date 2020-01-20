eff5c247510cf35eee922bc5f2cdafa903a448be
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-20 09:39:24 +0530

    [Divya]Refactor: the file handling operations

--- Datastructure/unorderedlinkedlist.py
+++ Datastructure/unorderedlinkedlist.py
@@ -1,75 +1,30 @@
-#node object class
-my_list = []
-with open("text","r") as f:
-    my_list = f.read().split()
-print(my_list)
-class Node:
-    def __init__(self,data=None,next=None):
-        self.data = data
-        self.next=next
-class linked_list:
-    def __init__(self):
-        self.head=None
-        self.count=0
-    def isEmpty(self):
-        return self.head==None
-    def add_node(self,data):
-        self.count += 1
-        new_node= Node(data)
-        if self.head==None:
-            self.head=new_node
-        else:
-            new_node.next=self.head
-            self.head=new_node
-    def traverse(self):
-        temp_node = self.head
-        while temp_node!=None:
-            print(temp_node.data,end=",")
-            temp_node = temp_node.next
-        print("NULL")
-    def search(self,item):
-        temp_node = self.head
-        Found = False
-        while temp_node!=None and not Found:
-            if temp_node.data == item:
-                Found = True
-            else:
-                temp_node = temp_node.next
-        return Found
-    def del_node(self,item,index=None):
-        self.count -= 1
-        temp_node = self.head
-        self.index = index
-        Found = False
-        while  not Found:
-            if temp_node.data == item:
-                Found = True
-            else:
-                self.index = temp_node
-                temp_node = temp_node.next
-        if temp_node.next==None:
-            self.head = temp_node.next
-        else:
-            self.index= temp_node.next
-    def count_Node(self):
-        print(self.count)
+import sys
+sys.path.append("/home/user/Desktop/programming/Datastructure")
+from DatastructureUtility import utilityoflinkedlist
 
-mylist=linked_list()
-for data in my_list:
-    mylist.add_node(data)
-mylist.traverse()
-word = input("enter key word:")
-if mylist.search(word)==False:
-    mylist.add_node(word)
-    with open("text","a") as f:
-        my_list = f.write(" "+word)
+
+mylist = []
+with open("text","r") as f:
+    mylist = f.read().split()
+print(mylist)
+
+
+myLinkedlList=utilityoflinkedlist.utility()
+print(myLinkedlList.isEmpty())
+for data in mylist:
+    myLinkedlList.add_node(data)
+word = input("serach word:")
+if myLinkedlList.search(word)==True:
+    myLinkedlList.remove(word)
+    for i in range(len(mylist)-1):
+        if word==mylist[i]:
+            del mylist[i]
 else:
-    mylist.del_node(word)
-    for i in range(len(my_list)):
-        if my_list[i] == word:
-            del my_list[i] 
-    with open('text', 'w') as f:
-        for listitem in my_list:
-            f.write('%s\n' % listitem)
-mylist.count_Node()
-print(my_list)
+    myLinkedlList.append(word)
+    mylist.append(word)
+myLinkedlList.traverse()
+print(mylist)
+with open("text",'w') as f:
+    for item in mylist:
+     f.write(item+"\n")
+
