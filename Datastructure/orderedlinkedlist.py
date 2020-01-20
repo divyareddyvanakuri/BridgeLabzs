04d9c8a42b6171c01b81d712f9f1cd435c69a145
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-20 15:52:36 +0530

    [Divya] Add: the functional logic of file Exception handling

--- Datastructure/orderedlinkedlist.py
+++ Datastructure/orderedlinkedlist.py
@@ -1,29 +1,34 @@
 import sys
 sys.path.append("/home/user/Desktop/programming/Datastructure")
 from DatastructureUtility import utilityoflinkedlist
+try:
+    mylist = []
+    with open("listofnumbers",'r') as f:
+        mylist = f.read().split()
 
-mylist = []
-with open("listofnumbers",'r') as f:
-    mylist = f.read().split()
+except FileNotFoundError:
+    print("Oops!  No such file or directory.  Try again...")
 
-
-Mylinkedlist = utilityoflinkedlist.utility()
-print(Mylinkedlist.isEmpty())
-for data in mylist:
-    Mylinkedlist.add_node(data)
-
-number = input("enter a number:")
-if Mylinkedlist.search(number)==True:
-    Mylinkedlist.remove(number)
-    for i in range(len(mylist)-1):
-        if str(number)==mylist[i]:
-            del mylist[i]
 else:
-    Mylinkedlist.add_node(number)
-    mylist.append(number)
-Mylinkedlist.sort()
-Mylinkedlist.traverse()
-print(mylist)
-with open("listofnumbers",'w') as f:
-    for item in mylist:
-     f.write(item+"\n")
+    Mylinkedlist = utilityoflinkedlist.utility()
+    print(Mylinkedlist.isEmpty())
+    for data in mylist:
+        Mylinkedlist.add_node(data)
+
+    number = input("enter a number:")
+    if Mylinkedlist.search(number)==True:
+        Mylinkedlist.remove(number)
+        for i in range(len(mylist)-1):
+            if str(number)==mylist[i]:
+                del mylist[i]
+    else:
+        Mylinkedlist.add_node(number)
+        mylist.append(number)
+    Mylinkedlist.sort()
+    Mylinkedlist.traverse()
+    print(mylist)
+    with open("listofnumbers",'w') as f:
+        for item in mylist:
+            f.write(item+"\n")
+finally:
+    print("executing finally clause")
