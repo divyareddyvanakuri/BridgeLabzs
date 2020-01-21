c5e37a4874459c0834554351ac4f82b69cbdab65
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-21 10:20:58 +0530

    [Divya]Refactor: The functional logic of deletion method

--- Datastructure/DatastructureUtility/utilityofCollisionResolutionwithchaining.py
+++ Datastructure/DatastructureUtility/utilityofCollisionResolutionwithchaining.py
@@ -1,7 +1,7 @@
 class utility:
     def __init__(self):
         self.sizeofhashtable=11
-        self.hashtable = [[] for _ in range(self.sizeofhashtable)]
+        self.hashtable = [0 for _ in range(self.sizeofhashtable)]
     def hashfunction(self,element):
         return element%self.sizeofhashtable
     def get_hashtable(self):
@@ -14,20 +14,26 @@ class utility:
             if type(self.hashtable[hash_key]) == list:
                 self.hashtable[hash_key].append(Value)
             else:
-                self.hashtable[hash_key] = [self.hashtable[hash_key], Value]
+                self.hashtable[hash_key] = [Value]
         
     def search(self,Value):
         hash_key=self.hashfunction(Value)
-        for i in range(len(self.hashtable[hash_key])):
-            if self.hashtable[hash_key][i]==Value:
+        if type(self.hashtable[hash_key]) == list:
+            for i in range(len(self.hashtable[hash_key])):
+                if self.hashtable[hash_key][i]==Value:
+                    return True
+                else:
+                    return False
+        else:
+            if self.hashtable[hash_key]==Value:
                 return True
             else:
                 return False
     def remove(self,Value):
         hash_key=self.hashfunction(Value)
         self.length = len(self.hashtable[hash_key])
-        if self.length==1:
-            self.hashtable[hash_key]=[]
+        if type(self.hashtable[hash_key])!= list:
+            self.hashtable[hash_key]=0
         else:
             for i in range(self.length-1):
                 if self.hashtable[hash_key][i]==Value:
