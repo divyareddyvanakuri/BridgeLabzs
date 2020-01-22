884531c4a3e955d7685754a27b1f526d13d71f16
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-22 11:06:14 +0530

    [Divya].refactor:the funactionlogic of primenumbers range

--- Datastructure/primeanagramsin2Darray.py
+++ Datastructure/primeanagramsin2Darray.py
@@ -1,5 +1,6 @@
+print("Enter range between 0-1000")
 lower = int(input("enter lower range value:"))
-upper = int(input("enter lower range value:"))
+upper = int(input("enter higher range value:"))
 
 def primeanagrams(lower,upper):
     prime_list=[]
@@ -16,10 +17,13 @@ def primeanagrams(lower,upper):
             if i != j and sorted(str(i))==sorted(str(j)):
                 anagram_list.append(j) 
     return anagram_list 
-mylist=primeanagrams(lower,upper)
-TwoDArray_anagramprime = [[x for x in range(100) if x in mylist],[x for x in range(100,200) if x in mylist],
-    [x for x in range(200,300) if x in mylist],[x for x in range(300,400) if x in mylist],
-    [x for x in range(400,500) if x in mylist],[x for x in range(500,600) if x in mylist],
-    [x for x in range(600,700) if x in mylist],[x for x in range(800,900) if x in mylist],
-    [x for x in range(900,1000) if x in mylist]]
-print(TwoDArray_anagramprime)
+if lower > upper:
+    print("Ensure Entered lower range value needs to be less the Entered higher range value")
+else:
+    mylist=primeanagrams(lower,upper)
+    TwoDArray_anagramprime = [[x for x in range(100) if x in mylist],[x for x in range(100,200) if x in mylist],
+        [x for x in range(200,300) if x in mylist],[x for x in range(300,400) if x in mylist],
+        [x for x in range(400,500) if x in mylist],[x for x in range(500,600) if x in mylist],
+        [x for x in range(600,700) if x in mylist],[x for x in range(800,900) if x in mylist],
+        [x for x in range(900,1000) if x in mylist]]
+    print(TwoDArray_anagramprime)
