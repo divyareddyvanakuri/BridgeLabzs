a9534ca5046aaa92e52411646bdbe0d03a6a1ab7
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-22 10:54:51 +0530

    [Divya]Refactor:the naming Convention

--- Datastructure/SimpleBalancedParentheses.py
+++ Datastructure/SimpleBalancedParentheses.py
@@ -3,16 +3,22 @@ sys.path.append("/home/user/Desktop/programming/Datastructure")
 from DatastructureUtility import utilityofstack
 
 mystack = utilityofstack.utility()
-a = input("Take Any Arithmetic Expression or Parentheses Expression:")
-print("Input:",a)
+p_expression = input("Take Any Arithmetic Expression or Parentheses Expression:")
+
 open_list = ["(","[","{"]
 closed_list = [")","]","}"]
-for i in range(len(a)):
-    if a[i] in open_list:
-        mystack.push(a[i])
-    elif a[i] in closed_list:
-        if mystack.size()>0:
+
+
+for i in range(len(p_expression)):
+    if p_expression[i] in open_list:
+        mystack.push(p_expression[i])
+    elif p_expression[i] in closed_list:
+        pos = closed_list.index(p_expression[i])
+        mylist = mystack.get_stack()
+        if mystack.size()>0 and open_list[pos] == mylist[len(mylist)-1]:
             mystack.Pop()
+        else:
+            mystack.push(p_expression[i])
 if mystack.is_empty():
     print("balanced")
 else:
