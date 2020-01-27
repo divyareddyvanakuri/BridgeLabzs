c68ffe3a90ea57b6c5cb646dde9105e720b8ac9d
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-27 18:56:51 +0530

    [Divya]Delete.deleted Duplicate Folder

--- basic programs/hormonicnumber.py
+++
@@ -1,5 +0,0 @@
-num = int(input("enter a harmonic number: "))
-harmonic = 1
-for i in range(2,num+1):
-    harmonic += 1/i
-print("{0}th harmonic value:{1} ".format(num,harmonic))
--- basic programs/prntusername.py
+++
@@ -1,5 +0,0 @@
-username = input("enter username: ")
-if len(username)<3:
-    print("Ensure username minimum three characters")
-else:
-    print("Hello {0},How are you?".format(username))
