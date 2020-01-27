c613e7bb055e8adb5e22cfd25f4dbc75fb4b8406
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-01-27 10:15:59 +0530

    [Divya]add: the function logic of remove person ,update person and creat new addressbook in the application

--- ObjectOrientedPrograms/Addressbook/addressbookapplication.py
+++ ObjectOrientedPrograms/Addressbook/addressbookapplication.py
@@ -1,44 +1,42 @@
 from addressbook import AddressBook
 from filesystem import Filesystem
 from personcls import Person
-myfile = Filesystem()
-# mybook = AddressBook()
+file_object = Filesystem()
 
 def read_person():
-        return {
-            'firstname': input("enter firstname:"),
-            'lastname': input("enter lastname:"),
-            'address': input("enter address:"),
-            'city': input("enter city:"),
-            'state': input("enter state:"),
-            'zipcode': input("enter zipcode:"),
-            'phonenumber': input("enter phonemuber:")
-        }
-
+    return {
+        'firstname': input("enter firstname:"),
+        'lastname': input("enter lastname:"),
+        'address': input("enter address:"),
+        'city': input("enter city:"),
+        'state': input("enter state:"),
+        'zipcode': input("enter zipcode:"),
+        'phonenumber': input("enter phonemuber:")
+    }
 def addPerson(addressbook):
     new_person = Person(read_person())
     addressbook.add_person(new_person)
-    myfile.saveFile("/home/user/Desktop/programming/ObjectOrientedPrograms/addressbookfile.json", addressbook)
+    file_object.saveFile("addressbookfile.json", addressbook)
 
+def updatePerson(addressbook):
+    index = int(input("enter index value:"))
+    update_person = Person(read_person())
+    addressbook.updateperson(index, update_person)
+    file_object.saveFile("addressbookfile.json", addressbook)
 
-def updatePerson():
-    return "two"
+def removePerson(addressbook):
+    index = int(input("enter index value:"))
+    addressbook.removeperson(index)
+    file_object.saveFile("addressbookfile.json", addressbook)
 
+def sortByName(addressbook):
+    addressbook.sortbyName()
 
-def removePerson():
-    pass
+def sortByZipCode(addressbook):
+    addressbook.sortbyZipcode()
 
-
-def sortByName():
-    pass
-
-
-def sortByZipCode():
-    pass
-
-
-def printAll():
-    pass
+def printAll(addressbook):
+    addressbook.showAll()
 
 
 print("Address Book Main-menu:")
@@ -55,10 +53,12 @@ while True:
         print("Handling Run-time error:", err)
     else:
         if option == 1:
-            pass
+            addressbook_object = file_object.readFile()
+            new_file_name = input("enter new file name")
+            file_object.saveFile(new_file_name+".json",addressbook_object)
 
         elif option == 2:
-            addressbook_object = myfile.readFile()
+            addressbook_object = file_object.readFile()
             print(" 1.Add a person"), print(
                 " 2.Edit a person"), print(" 3.Delete a person")
             print(" 4.sort Entries by Name"), print(
@@ -73,16 +73,16 @@ while True:
             else:
                 def switch_demo(choice):
                     switcher = {
-                        1: addPerson(addressbook_object),
-                        2: updatePerson(),
-                        3: removePerson(),
-                        4: sortByName(),
-                        5: sortByZipCode(),
-                        6: printAll()
+                        1: addPerson,
+                        2: updatePerson,
+                        3: removePerson,
+                        4: sortByName,
+                        5: sortByZipCode,
+                        6: printAll
                     }
                     return switcher.get(choice, lambda: "invalide choice,Ensure option will be 1-6")
                 func = switch_demo(choice)
-                
+                print(func(addressbook_object))
 
         elif option == 3:
             break
