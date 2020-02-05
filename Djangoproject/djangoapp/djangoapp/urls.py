33c83f1e4bc419f1966c21c221fb9a9d890b058b
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-05 14:49:00 +0530

    [Divya].add: Add the django project files

---
+++ Djangoproject/djangoapp/djangoapp/urls.py
@@ -0,0 +1,40 @@
+"""djangoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls import url
+from django.contrib.auth.models import User
+from chatapp import views
+from chatapp.views import LoginApiView,RegisterApiView,PasswordResetApiView
+#from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    #path('home/',views.home),
+    #path('api-auth/',include('rest_framework.urls')),
+    #url(r'^api-token-auth/',views.obtain_auth_token),
+    #path('register/',CreateUserApiView.as_view()),
+    path('login/',LoginApiView.as_view()),
+    path('register/',RegisterApiView.as_view()),
+    path('login/PasswordResetApiView/',PasswordResetApiView.as_view()),
+    # path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    # path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('activate/<slug:surl>/',views.activate,name='activate'),
+    # path('login/passwordactivate/',views.passwordactivate)
+]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
